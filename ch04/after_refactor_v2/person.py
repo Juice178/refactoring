187a918e85dd2eb683b264a02011ef917fc1709b
Kenta <kenasuzuki5555@gmail.com>
2021-02-14 22:50:45 +0900

    feat: Add source code for chapter4

---
+++ ch04/after_refactor_v2/person.py
@@ -0,0 +1,15 @@
+from label import Label
+# from null_label import NullLabel
+
+
+class Person:
+    def __init__(self, name: Label = Label.new_null(), mail: Label = Label.new_null()) -> None:
+        self._name = name 
+        self._mail = mail 
+
+    def display(self) -> None:
+        self._name.display()
+        self._mail.display()
+
+    def __str__(self) -> str:
+        return f"[ Person: name= {self._name} mail= {self._mail} ]"
