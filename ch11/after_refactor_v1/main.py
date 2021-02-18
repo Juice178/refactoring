4030127db8185f026a1aaca1c31a00792635a4f9
Kenta <kenasuzuki5555@gmail.com>
2021-02-19 00:24:07 +0900

    refactor: Add refactored code (ver1) for ch11

---
+++ ch11/after_refactor_v1/main.py
@@ -0,0 +1,15 @@
+from shape import Shape
+
+
+def main() -> None:
+    line = Shape.create(Shape.TYPECODE_LINE, 0, 0, 100, 200)
+    rectangle= Shape.create(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40)
+    oval = Shape.create(Shape.TYPECODE_OVAL, 100, 200, 300, 400)
+
+    shape = [line, rectangle, oval]
+    for s in shape:
+        s.draw()
+
+
+if __name__ == '__main__':
+    main()
